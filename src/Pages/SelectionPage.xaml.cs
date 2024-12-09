3d0c2ddf92d02eb8076f992af7cf824d96f462d8
Waheed Ahmad <ewammuslim@outlook.com>
2024-12-10 02:44:46 +0500

    Update SelectionPage.xaml.cs

--- src/Pages/SelectionPage.xaml.cs
+++ src/Pages/SelectionPage.xaml.cs
@@ -1,6 +1,4 @@
-using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Data;
 
 namespace WinUI.TableView.SampleApp.Pages;
 
@@ -9,17 +7,5 @@ public sealed partial class SelectionPage : Page
     public SelectionPage()
     {
         InitializeComponent();
-
-        Loaded += SelectionPage_Loaded;
-    }
-
-    private async void SelectionPage_Loaded(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
-    {
-        await Task.Delay(5000);
-
-        tableView.Columns =
-        [
-            new TableViewTextColumn {Header  = "Id", Binding = new Binding{Path = new PropertyPath("Id")}}
-        ];
     }
 }
